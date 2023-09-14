5851c8385aaf21794b14227539e326cac07ee418
Code-Apprentice <codeguru42@gmail.com>
2023-09-14 17:09:05 -0600

    Implement root route to return customization

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {
+      "apiversion": "1",
+      "author": "codeguru42",
+      "color": "#B62B2B",
+      "head": "default",
+      "tail": "default",
+      "version": "0.0.1-beta"
+    }
 
 
 @app.get("/hello/{name}")
