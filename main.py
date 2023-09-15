c2a18048bc4a0300e36b57b96176b20b8b4209d5
Code-Apprentice <codeguru42@gmail.com>
2023-09-14 18:04:21 -0600

    /start route

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ async def root():
     }
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/start")
+async def start():
+    return {
+        "message": "starting"
+    }
