749552103a34a0cbcbb565408785a756137568e6
Code-Apprentice <codeguru42@gmail.com>
2023-09-18 23:38:28 -0600

    Apply black formatting

--- main.py
+++ main.py
@@ -8,32 +8,26 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {
-      "apiversion": "1",
-      "author": "codeguru42",
-      "color": "#B62B2B",
-      "head": "default",
-      "tail": "default",
-      "version": "0.0.1-beta"
+        "apiversion": "1",
+        "author": "codeguru42",
+        "color": "#B62B2B",
+        "head": "default",
+        "tail": "default",
+        "version": "0.0.1-beta",
     }
 
 
 @app.post("/start")
 async def start():
-    return {
-        "message": "starting"
-    }
+    return {"message": "starting"}
 
 
 @app.post("/move")
 async def move():
     moves = ["up", "down", "left", "right"]
-    return {
-        "move": random.choice(moves)
-    }
+    return {"move": random.choice(moves)}
 
 
 @app.post("/end")
 async def end():
-    return {
-        "message": "ending"
-    }
+    return {"message": "ending"}
