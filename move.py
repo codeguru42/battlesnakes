e410acbca9dfa94163171ba5470d0c8bace8b626
Code-Apprentice <codeguru42@gmail.com>
2023-09-19 22:29:37 -0600

    Refactor all_dist() method

--- move.py
+++ move.py
@@ -7,11 +7,15 @@ def dist(p1: Position, p2: Position) -> int:
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 
+def all_dist(ps: list[Position], x: Position):
+    return [dist(x, p) for p in ps]
+
+
 def make_move(state: GameState) -> MoveEnum:
     head = state.you.head
     food = state.board.food
     deltas = {m: m.delta() for m in MoveEnum}
     choices = {m: Position(x=head.x + d.x, y=head.y + d.y) for m, d in deltas.items()}
-    min_dist = {m: min(dist(c, f) for f in food) for m, c in choices.items()}
+    min_dist = {m: min(all_dist(food, c)) for m, c in choices.items()}
 
     return min(min_dist.keys(), key=lambda m: min_dist[m])
