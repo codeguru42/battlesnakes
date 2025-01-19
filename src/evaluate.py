14b55f6a94fad0357f8969ac32a39e8975e96cac
Code-Apprentice <codeguru42@gmail.com>
2025-01-18 17:45:45 -0700

    Use simple evaluate() function
    Calculates minimum distance to food

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,14 +1,16 @@
+from typing import Iterator, Tuple
+
 import networkx as nx
 
-from models import GameState, Position
-from move import make_graph
+from models import GameState, Position, MoveEnum
 
 
-def evaluate(state: GameState) -> int:
+def evaluate(move: MoveEnum, state: GameState) -> int:
     head = state.you.head
     food = state.board.food
     graph = make_graph(state)
-    return -min_dist(food, head, graph)
+    new_pos = head + move
+    return -min_dist(food, new_pos, graph)
 
 
 def min_dist(ps: list[Position], x: Position, graph: nx.DiGraph):
@@ -21,3 +23,36 @@ def all_dist(ps: list[Position], x: Position, graph: nx.DiGraph):
             yield nx.shortest_path_length(graph, x, p)
         except (nx.NetworkXNoPath, nx.NodeNotFound):
             yield 1000000
+
+
+def make_graph(state: GameState) -> nx.DiGraph:
+    graph: nx.DiGraph = nx.DiGraph()
+    used = set().union(*(s.body[:-1] for s in state.board.snakes))
+    to_visit = [state.you.head]
+    visited = set()
+    while len(to_visit) > 0:
+        curr = to_visit.pop()
+        visited.add(curr)
+        for _, neighbor in neighbors(curr):
+            if is_in_bounds(
+                neighbor, state.board.width, state.board.height
+            ) and not is_occupied(neighbor, used):
+                graph.add_edge(curr, neighbor)
+                if neighbor not in visited:
+                    to_visit.append(neighbor)
+    return graph
+
+
+def neighbors(pos: Position) -> Iterator[Tuple[MoveEnum, Position]]:
+    for m in MoveEnum:
+        yield m, Position(
+            x=pos.x + MoveEnum(m).delta().x, y=pos.y + MoveEnum(m).delta().y
+        )
+
+
+def is_in_bounds(pos: Position, width: int, height: int) -> bool:
+    return 0 <= pos.x < width and 0 <= pos.y < height
+
+
+def is_occupied(pos: Position, used: set[Position]) -> bool:
+    return pos in used
--- src/move.py
+++ src/move.py
@@ -1,9 +1,6 @@
 from collections.abc import Iterator
-from typing import Tuple
 
-import networkx as nx
-
-from evaluate import min_dist
+from evaluate import evaluate
 from models import GameState, MoveEnum, Position, Battlesnake
 
 
@@ -11,36 +8,12 @@ class InvalidMove(BaseException):
     pass
 
 
+def make_move(state: GameState) -> MoveEnum:
+    return min((MoveEnum(m) for m in MoveEnum), key=lambda m: evaluate(m, state))
+
+
 def dist(p1: Position, p2: Position) -> int:
     return abs(p1.x - p2.x) + abs(p1.y - p2.y)
-
-
-def make_move(state: GameState) -> MoveEnum:
-    head = state.you.head
-    food = state.board.food
-    snakes = state.board.snakes
-    used = set().union(*(s.body[:-1] for s in snakes))
-    choices = {}
-    other_snakes = set(snakes) - {state.you}
-    for m, new_pos in neighbors(head):
-        if (
-            is_in_bounds(new_pos, state.board.width, state.board.height)
-            and not is_occupied(new_pos, used)
-            and will_win_head_to_head(new_pos, state.you, other_snakes)
-        ):
-            choices[m] = new_pos
-    graph = make_graph(state)
-    md = {m: min_dist(food, c, graph) for m, c in choices.items()}
-
-    return min(md.keys(), key=lambda m: md[m])
-
-
-def is_in_bounds(pos: Position, width: int, height: int) -> bool:
-    return 0 <= pos.x < width and 0 <= pos.y < height
-
-
-def is_occupied(pos: Position, used: set[Position]) -> bool:
-    return pos in used
 
 
 def move_snake(snake: Battlesnake, move: MoveEnum) -> Position:
@@ -87,28 +60,3 @@ def will_win_head_to_head(
         not is_head_neighbor(other, pos) or is_longer(you, other)
         for other in other_snakes
     )
-
-
-def neighbors(pos: Position) -> Iterator[Tuple[MoveEnum, Position]]:
-    for m in MoveEnum:
-        yield m, Position(
-            x=pos.x + MoveEnum(m).delta().x, y=pos.y + MoveEnum(m).delta().y
-        )
-
-
-def make_graph(state: GameState) -> nx.DiGraph:
-    graph: nx.DiGraph = nx.DiGraph()
-    used = set().union(*(s.body[:-1] for s in state.board.snakes))
-    to_visit = [state.you.head]
-    visited = set()
-    while len(to_visit) > 0:
-        curr = to_visit.pop()
-        visited.add(curr)
-        for _, neighbor in neighbors(curr):
-            if is_in_bounds(
-                neighbor, state.board.width, state.board.height
-            ) and not is_occupied(neighbor, used):
-                graph.add_edge(curr, neighbor)
-                if neighbor not in visited:
-                    to_visit.append(neighbor)
-    return graph
